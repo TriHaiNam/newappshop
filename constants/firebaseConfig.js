1ec97146876c4efd47a07669d4a0f39430f66732
ngndevil <nguyen24122002@gmail.com>
2025-04-09 22:38:56 +0700

    1

--- constants/firebaseConfig.js
+++ constants/firebaseConfig.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC5gmvSNNdazFFJZvOvkwCKv58BcUMjJxU",
   authDomain: "clothing-store-1165d.firebaseapp.com",
   projectId: "clothing-store-1165d",
-  storageBucket: "clothing-store-1165d.appspot.com", // ✅ sửa ".app" thành ".app**spot.com**"
+  storageBucket: "clothing-store-1165d.appspot.com",
   messagingSenderId: "276500725099",
   appId: "1:276500725099:web:26e14c3d98897cbd4ca02e",
 };
